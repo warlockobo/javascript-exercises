d7abe4c9e8a71b13a1c18f0cd897b9b338cf9ba9
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-29 04:29:44 -0600

    learned what filter was and used it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removedValue) {
+    return array.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
