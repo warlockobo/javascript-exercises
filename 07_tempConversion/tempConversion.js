f57d3736c3138ba326fd390bad7e5e71f3242f65
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-30 01:38:31 -0600

    parse float

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(tempF) {
-  return (tempF - 32) * (5/9);
+  return parseFloat(((tempF - 32) * (5/9)).toFixed(1));
 };
 
 const convertToFahrenheit = function(tempC) {
- return (tempC * (9/5)) + 32;
+ return parseFloat(((tempC * (9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
