118dfb6d72e698209b8f24ec7c2ee559fc0952ad
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-05 02:55:23 -0600

    completed to factorial on calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,16 +6,16 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(a, b) {
-
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
 const factorial = function() {
