2f74aeb029a2f4ee7c214c49d898050c035ec07c
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-05 03:05:16 -0600

    caluclator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,12 @@ const power = function(a, b) {
 	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
