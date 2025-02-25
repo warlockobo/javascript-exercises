009edd54e81871d2964a4d61bcc8b57d8e80a073
Wittner Andrews <wittnerandrews@gmail.com>
2025-02-25 01:48:27 -0600

    removed skips and coded sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    let fib = [0, 1]; // Initialize array!
+    
+    for (i = 2; i <= count; i++) {
+      // Next fibonacci number = previous + one before previous
+      // Translated to JavaScript:
+      fib[i] = fib[i - 2] + fib[i - 1];
+    };
+
+    return fib[count];
 
 };
 
